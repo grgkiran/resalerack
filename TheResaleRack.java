214bb6cc5135cbcc5b1c85763c6af881ed76fb5c
grgkiran <145780331+grgkiran@users.noreply.github.com>
2023-09-22 19:41:27 +0545

    Add files via upload

---
+++ TheResaleRack.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package the.resale.rack;
+
+/**
+ *
+ * @author Gurung_Kiran
+ */
+public class TheResaleRack {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
